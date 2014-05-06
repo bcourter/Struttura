02c68099025c1e301d3cf7f4221bad797d5088f0
bcourter <git-bcourter.gmail.com>
2014-05-05 22:03:19 -0400

    reading SpaceClaim JSON

--- js/struttura.js
+++ js/struttura.js
@@ -1,4 +1,6 @@
-var renderer, camera, settings, container3D, panels, geometry, lightGeometry;
+var renderer, camera, settings, container3D, panels
+var material, geometry;
+var mesh, edgeMesh;
 var objModel = [];
 var objModelCount = 3;
 var lastTime = 0, lastAnimation = 0, lastRotation = 0;
@@ -49,49 +51,27 @@ function init() {
     window.addEventListener('resize', onWindowResize, false);
 
     var Settings = function () {
-		this.show4dExplode = document.getElementById("show4dExplode");
-		this.is4dExplode = document.getElementById("is4dExplode");
-		this.showHyperbolic = document.getElementById("showHyperbolic")
-		this.isHyperbolic = document.getElementById("isHyperbolic");
-
-
         document.getElementById("saveObj").onclick = saveObj;
     };
 
-      //  var prefix = 'resources/obj/4-5.37-';
-      //  var prefix = 'resources/obj/4-5.40-';
-        var prefix = 'resources/obj/4-5.48-lacy-';
-        var extension = '.stl';
+    var patternFile = 'resources/json3D/cylinder.js';
 
-        var loader0 = new THREE.STLLoader();
-        loader0.addEventListener('load', function (event) { objModel[0] = basicMeshFromGeometry(loadGeometry(event.content)); });
-        loader0.load(prefix + 0 + extension);
-    
-        var loader1 = new THREE.STLLoader();
-        loader1.addEventListener('load', function (event) { objModel[1] = basicMeshFromGeometry(loadGeometry(event.content)); });
-        loader1.load(prefix + 1 + extension);
-    
-        var loader2 = new THREE.STLLoader();
-        loader2.addEventListener('load', function (event) { objModel[2] = basicMeshFromGeometry(loadGeometry(event.content)); });
-        loader2.load(prefix + 2 + extension);
-    
+    loadpart( patternFile, function ( geometries, edges ) { loadGeometry(geometries, edges); } );
+
     settings = new Settings();
 }
 
-function loadGeometry(geometry) {
-    geometry.computeBoundingBox();
-    var box = new THREE.Box3();
-    box.union(geometry.boundingBox);  //TBD does this accidentaly cause it to include zero from the first time?
-    var center = box.center();
-//      var scale = box.size().length() * 0.5;
-    var scale = 1;
-    var vertices = geometry.vertices;
-    for (var i = 0; i < vertices.length; i++) {  
-        geometry.vertices[i] = vertices[i].sub(center).multiplyScalar(scale);
-    }
+function loadGeometry(geometries, lines) {
+    var g = geometries.shift();
+    while (geometries.length > 0)
+        THREE.GeometryUtils.merge(g, geometries.shift());
 
-    return geometry;
-}
+    g.mergeVertices();
+    geometry = g;
+
+    edgeMesh = lines;
+ }
+
 
 function onWindowResize() {
     var width = container3D.offsetWidth;
@@ -108,216 +88,6 @@ function animate() {
     render();
     controls.update();
     //stats.update();
-}
-
-var circleToStrip = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-    z = Complex.atanh(z).scale(4 / Math.PI);  // butulov says 2 * pi, my old notes this.  what up?  Doesn't seem to matter yet...
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-// http://bulatov.org/math/1001/
-// w=(12)(-alog(1-az)-log(1-z))
-var circleToHeartStrip = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-    var a = Complex.createPolar(1, Math.PI / 8);
-    // z = Complex.add(
-    //         Complex.multiply(
-    //             a,
-    //             Complex.atanh(
-    //                 Complex.multiply(a,z)
-    //             )
-    //         ),
-    //         Complex.atanh(z)
-    //     );
-
-    //z = Complex.add(z, Complex.i);
-
-    var cutoff = 0.99996
-    z = Complex.subtract(
-        Complex.multiply(a, Complex.log(Complex.add(Complex.one, Complex.multiply(a.conjugate().scale(cutoff), z)))),  
-        Complex.multiply(a.conjugate(), Complex.log(Complex.subtract(Complex.one, Complex.multiply(a.scale(cutoff), z))))
-        ).scale(0.5);
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-// http://bulatov.org/math/1003/index_ring.html
-// w=exp(za)
-var stripToAnnulus = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    var a =  Math.PI / 2.2788701240774127 / 2
-    z = Complex.exp(z.scale(a));
-
-    vertex.x = z.re;
-    vertex.y = z.im;
- //   vertex.z = vertex.z + Complex.exp(z.scale(a * 2)).re;
-    return vertex;
-};
-
-var stripToAnnulusZ = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    var a =  Math.PI / 2.2788701240774127 / 2.5;
-    z = Complex.exp(z.scale(a));
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    var s = Math.cos(z.argument() * 2.5 ) * 0.2;
-    vertex.z += s * z.modulus() * 2.5/2;
-//    vertex.z += );
-   // vertex.z += (Math.atan2(z.im, z.re));
- //   vertex.z = vertex.z + Complex.exp(z.scale(a * 2)).re;
-    return vertex;
-};
-
-
-
-var rotate = function(vertex, angle) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    var rotation = Mobius.createRotation(angle);
-    z = z.transform(rotation);
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-var squarify = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    z = Complex.sqrt(Complex.divide(
-            Complex.multiply(
-                Complex.i,
-                Complex.subtract(Complex.one, z)
-            ),
-            Complex.add(z, Complex.one)
-        ));
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-
-
-var halfplane = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    z = Complex.divide(
-            Complex.multiply(
-                Complex.i,
-                Complex.subtract(Complex.one, z)
-            ),
-            Complex.add(z, Complex.one)
-        );
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-var halfstrip = function(vertex) {             
-    var z = new Complex(vertex.x, vertex.y);
-
-    z = Complex.acosh(z);
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-var translate = function(vertex, point, angle) {             
-    var z = new Complex(vertex.x, vertex.y);
-    var a = new Complex(point.x, point.y);
-
-    var translation = Mobius.createDiscAutomorphism(a, angle);
-    z = z.transform(translation).conjugate();
-
-    vertex.x = z.re;
-    vertex.y = z.im;
-    return vertex;
-};
-
-var offset = 0.25 / 25.4 / 0.13;
-var offsetZOnly = 0.5;
-var roll = function(vertex, n) {         
-    var sign = vertex.z > 0 ? 1 : -1;
-    if (vertex.z * sign < 0.01)
-        sign = 0;
-
-    var period = 1.1394350620387064 * 4;
-    var radius = n * period / 2 / Math.PI;
-
-    var thickness = offset * offsetZOnly ;  //tiara
-
-    var phi = vertex.x / radius;
-    var dist = radius + offset/3*0 + vertex.z / 2;
-    vertex.z = dist * Math.cos(phi);
-    vertex.x = dist * Math.sin(phi);
-
- //   vertex.z = vertex.x * 1/Math.tan(Math.asin(vertex.x / radius));
-    return vertex;
-};
-
-
-var rollRing = function(vertex, n, sign, scale) {         
-    var period = 1.1394350620387064 * 4;
-    var radius = n * period / 2 / Math.PI;
-
-    var thickness = offset * offsetZOnly * 2.5; // ring
- //   var thickness = offset * offsetZOnly ;  //tiara
-
-    var bottomScale = 0.2;
-    var radiusOffset = 0;
-    if (sign == -1) {
-        if (vertex.z < -0.02) {
-            radiusOffset = thickness;
-        } else {
-            radiusOffset = 0;
-        }
-    }
-
-    if (sign == 1) {
-        if (vertex.z < -0.02) {
-            radiusOffset = -thickness;
-        } else {
-            radiusOffset = bottomScale * thickness;
-        }
-
-        radiusOffset += thickness;
-    }
-
-    var depth = vertex.z * Math.cos(vertex.y * Math.PI / 2) * scale;
-    var phi = vertex.x / radius;
-    var dist = radius + sign * depth * 2 + radiusOffset;
-    vertex.z = dist * Math.cos(phi);
-    vertex.x = dist * Math.sin(phi);
-
- //   vertex.z = vertex.x * 1/Math.tan(Math.asin(vertex.x / radius));
-    return vertex;
-};
-
-
-var scale = function(vertex) {         
-    vertex.multiplyScalar(0.13);
-    return vertex;
-};
-
-function linearToHyperbolic(x) {
-    x++;  // to scale...
-    return Math.log(x + Math.sqrt(x * x - 1)); // arcosh(x)
-}
-
-function basicMeshFromGeometry(geometry) {
-    return new THREE.Mesh(geometry, new THREE.MeshLambertMaterial());
 }
 
 function mergeAllVertices(object3D) {
@@ -345,26 +115,11 @@ function render() {
     var time = new Date().getTime() / 1000;
     lastTime = time;
 
-    for (var i = 0; i < objModelCount; i++) {
-        if (objModel[i] === undefined)
-            return;
-    }
-
-    var thickness = offset / 4 * (1 - offsetZOnly); //  / 2 * (1 + Math.sin(time));
     if (geometry === undefined) {
-        var objGeometry = [];
-        for (var i = 0; i < objModelCount; i++) {
-            objGeometry[i] = mergeAllVertices(objModel[i])
-        }
-
-
-       geometry =  objGeometry[0];
-
-
+        return;
     }
 
     var animGeometry = geometry;
-
  
     material = [
         new THREE.MeshPhongMaterial( { 
